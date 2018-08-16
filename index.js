41f81cba05e22c4c34971e292d969adf80c0309a
xiang.wang <xiang.wang@yintech.cn>
2018-08-16 15:27:58 +0800

    add message api

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict'
 
 var API = require('./lib/api_common')
-// 模板消息
-API.mixin(require('./lib/api_template'))
 // 登录
 API.mixin(require('./lib/api_login'))
+// 模板消息
+API.mixin(require('./lib/api_template'))
+// 客服消息
+API.mixin(require('./lib/api_message'))
 
 module.exports = API
